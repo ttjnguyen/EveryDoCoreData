7bc5873d4be5beff9a466a3b7d4b1aebf03989b6
ttjnguyen <ttjnguyen6@gmail.com>
2016-11-23 11:56:28 -0800

    Initial Commit

---
+++ EveryDoCoreData/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  EveryDoCoreData
+//
+//  Created by Jenny Nguyen on 2016-11-23.
+//  Copyright © 2016 LightHouseLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ EveryDoCoreData/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  EveryDoCoreData
+//
+//  Created by Jenny Nguyen on 2016-11-23.
+//  Copyright © 2016 LightHouseLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
